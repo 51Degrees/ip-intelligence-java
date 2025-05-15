5d31ac4f064ceb1e2a7b1d9c481d603ed2e3c080
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-15 15:43:09 +0300

    CI: Make `PreBuild.sh` executable.

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -8,4 +8,14 @@ param(
 
 ./java/setup-enviroment.ps1 -RepoName $RepoName -ProjectDir $ProjectDir -JavaSDKEnvVar $JavaSDKEnvVar
 
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
+
+if ($IsLinux -or $IsMacOS) {
+    Write-Output "Marking PreBuild.sh as executable..."
+    chmod +x ip-intelligence.engine.on-premise/src/main/cxx/PreBuild.sh
+    Write-Output "Marking PreBuild.sh as executable -- done."
+}
+
 exit $LASTEXITCODE
