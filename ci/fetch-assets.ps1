76cad5f95a0739b944c561aefe1c365a0b3f6d36
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-15 15:07:41 +0300

    CI: Update `fetch-assets.ps1`.

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,31 +1,17 @@
 
 param (
-    [Parameter(Mandatory=$true)]
     [string]$RepoName,
     [Parameter(Mandatory=$true)]
-    [string]$IPIntelligence,
-    [string]$IPIntelligenceUrl
+    [string]$DeviceDetection,
+    [string]$DeviceDetectionUrl
 )
 
-$RepoPath = [IO.Path]::Combine($pwd, $RepoName)
+$CxxCiDir = Join-Path $RepoName "ip-intelligence.engine.on-premise" "src" "main" "cxx" "ip-intelligence-cxx" "ip-intelligence-data"
+$CxxCiScript = Join-Path $pwd $CxxCiDir "ci" "fetch-assets.ps1"
 
-./steps/fetch-hash-assets.ps1 -RepoName $RepoName -LicenseKey $IPIntelligence -Url $IPIntelligenceUrl
+& $CxxCiScript `
+    -RepoName $CxxCiDir `
+    -DeviceDetection $DeviceDetection `
+    -DeviceDetectionUrl $DeviceDetectionUrl
 
-Write-Output "Download Lite file"
-curl -L -o "$RepoPath/51Degrees-LiteV4.1.hash" "https://github.com/51Degrees/ip-intelligence-data/raw/main/51Degrees-LiteV4.1.hash"
-
-Write-Output "Download Evidence file"
-curl -o "$RepoPath/20000 Evidence Records.yml" "https://media.githubusercontent.com/media/51Degrees/ip-intelligence-data/main/20000%20Evidence%20Records.yml"    
-
-Write-Output "Download User Agents file"
-curl -o "$RepoPath/20000 User Agents.csv" "https://media.githubusercontent.com/media/51Degrees/ip-intelligence-data/main/20000%20User%20Agents.csv"
-
-
-Copy-Item "$RepoPath/20000 Evidence Records.yml"  "$RepoPath/ip-intelligence.engine.on-premise/src/main/cxx/ip-intelligence-cxx/ip-intelligence-data/20000 Evidence Records.yml"
-
-Copy-Item "$RepoPath/20000 User Agents.csv"  "$RepoPath/ip-intelligence.engine.on-premise/src/main/cxx/ip-intelligence-cxx/ip-intelligence-data/20000 User Agents.csv"
-
-Copy-Item $RepoPath/51Degrees-LiteV4.1.hash  $RepoPath/ip-intelligence.engine.on-premise/src/main/cxx/ip-intelligence-cxx/ip-intelligence-data/51Degrees-LiteV4.1.hash
-
-Copy-Item $RepoPath/TAC-HashV41.hash  $RepoPath/ip-intelligence.engine.on-premise/src/main/cxx/ip-intelligence-cxx/ip-intelligence-data/TAC-HashV41.hash
-
+exit $LASTEXITCODE
