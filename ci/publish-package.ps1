737ded3bab08297d2105d3cd130d300c301794aa
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-27 12:54:14 +0300

    Revert "CI: Disable publishing to Maven."
    This reverts commit ecbb340ebb5bdca263142285d109b841699bfcc9.

--- ci/publish-package.ps1
+++ ci/publish-package.ps1
@@ -4,10 +4,7 @@ param(
     [Parameter(Mandatory)][hashtable]$Keys
 )
 
-Write-Error "Disabled till ready to release."
-exit 1
-
-# ./java/publish-package-maven.ps1 -RepoName $RepoName -MavenSettings $Keys['MavenSettings'] -Version $Version
+./java/publish-package-maven.ps1 -RepoName $RepoName -MavenSettings $Keys['MavenSettings'] -Version $Version
 
 
 exit $LASTEXITCODE
