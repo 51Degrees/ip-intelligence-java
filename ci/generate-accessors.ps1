86f97029ccf662d7db01f5dbff9d05dc0a1060b6
Maksym Kucherov <maximus.the0@gmail.com>
2025-05-30 14:06:46 +0300

    CI: Fix data file update? pt.3

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -6,5 +6,5 @@ $ErrorActionPreference = "Stop"
 
 ./tools/ci/generate-accessors.ps1 @PSBoundParameters
 
-Copy-Item "tools/Java/IPIDataBase.java" "ip-intelligence-java/ip-intelligence.shared/src/main/java/fiftyone/ipintelligence/shared/"
+Copy-Item "tools/Java/IPIntelligenceDataBase.java" "ip-intelligence-java/ip-intelligence.shared/src/main/java/fiftyone/ipintelligence/shared/"
 Copy-Item "tools/Java/IPIntelligenceData.java" "ip-intelligence-java/ip-intelligence.shared/src/main/java/fiftyone/ipintelligence/shared/"
