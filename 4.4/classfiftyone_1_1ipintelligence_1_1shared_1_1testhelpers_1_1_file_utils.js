2ae6720655eed7752e60dd9c89d3b8912faea2d1
Automation51D <51DCI@51Degrees.com>
2025-06-12 03:29:08 +0000

    Update documentation from main

--- 4.4/classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.js
+++ 4.4/classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.js
@@ -5,9 +5,9 @@ var classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils =
     [ "getHashFileName", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#abb36d0ea9230b1417ea823755beb1ae7", null ],
     [ "jarFileHelper", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#aeee57e0cf0cef2ce20cd1423ad8d52ff", null ],
     [ "ENTERPRISE_IPI_DATA_FILE_NAME", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#ac198d58ae3fe6aeb2f137c8c129ba857", null ],
+    [ "ENTERPRISE_IPI_V41_DATA_FILE_NAME", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#a43059f36ec787036589481d517a41ce2", null ],
     [ "EVIDENCE_FILE_NAME", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#a5bf3ac945ca3923a7c7183a33514957f", null ],
     [ "IP_ADDRESSES_FILE_NAME", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#aa6a2e42b3ba00448aea4ed54fd6200bf", null ],
     [ "LITE_IPI_DATA_FILE_NAME", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#adc4eb3b4478d41dc9a628fd3d93b41f5", null ],
-    [ "TAC_IPI_DATA_FILE_NAME", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#ad9b0c18e6edfb9d3198cb399fd36fc91", null ],
     [ "TEMP_FILE_PREFIX", "classfiftyone_1_1ipintelligence_1_1shared_1_1testhelpers_1_1_file_utils.html#af3457f7693fb2ce56424612cfd8990be", null ]
 ];
