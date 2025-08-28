c85db26df6f1f6bf4db2d86f5fc41ab89b96066f
Automation51D <51DCI@51Degrees.com>
2025-08-28 03:23:38 +0000

    Update documentation from main

--- 4.5/examples.js
+++ 4.5/examples.js
@@ -1,6 +1,7 @@
 var examples =
 [
     [ "console/GettingStartedOnPrem.java", "console_2_getting_started_on_prem_8java-example.html", null ],
+    [ "console/MetadataOnPrem.java", "console_2_metadata_on_prem_8java-example.html", null ],
     [ "console/OfflineProcessing.java", "console_2_offline_processing_8java-example.html", null ],
     [ "console/UpdateDataFile.java", "console_2_update_data_file_8java-example.html", null ],
     [ "GettingStartedWebOnPrem.java", "_getting_started_web_on_prem_8java-example.html", null ],
