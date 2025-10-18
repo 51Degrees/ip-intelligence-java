a24705c39518b72d7468910b6618df872b16aa37
Automation51D <51DCI@51Degrees.com>
2025-10-18 03:16:14 +0000

    Update documentation from main

--- 4.5/classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.js
+++ 4.5/classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.js
@@ -5,5 +5,6 @@ var classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper =
     [ "getDataFileLocation", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.html#adad2e7d9b01160aea0b11520980c7727", null ],
     [ "getDatafileMetaData", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.html#aa1cb12aa53656a59898abffe4f3f14de", null ],
     [ "getEvidenceFile", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.html#ac3d02f7917c7c4630ffa49d23e1c6cdc", null ],
-    [ "logDataFileInfo", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.html#aa3f4734172328c2b9365c5719c5564e1", null ]
+    [ "logDataFileInfo", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.html#aa3f4734172328c2b9365c5719c5564e1", null ],
+    [ "ENTERPRISE_DATA_FILE_REL_PATH", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_data_file_helper.html#ad902ce64cb85a95b86ea524c725a589f", null ]
 ];
--- 4.5/classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.js
+++ 4.5/classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.js
@@ -3,6 +3,7 @@ var classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper =
     [ "asFloatProperty", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.html#ae38fe3da59dde2fb365a016f3f9e0c79", null ],
     [ "asIntegerProperty", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.html#a535d88576d711d2010ea35b0ca711ac9", null ],
     [ "asIPAddressProperty", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.html#a5dbdf0193bd388e30ca7b2da115f2301", null ],
+    [ "asString", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.html#a38725a3937d033b008aeadf3e3af8f65", null ],
     [ "asStringProperty", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.html#ac71dc498768a0f7960fffe6149183ab0", null ],
     [ "tryGet", "classfiftyone_1_1ipintelligence_1_1examples_1_1shared_1_1_property_helper.html#a2b93453e66ade2f9e8b350523e18b068", null ]
 ];
