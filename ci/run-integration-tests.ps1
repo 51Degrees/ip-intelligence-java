00e27caf144a3b05c32b193b00753020a4303d92
Eugene Dorfman <eugene.dorfman@gmail.com>
2025-10-20 18:50:11 +0200

    FIX: run integration tests on publishing
    use correct secrets

--- ci/run-integration-tests.ps1
+++ ci/run-integration-tests.ps1
@@ -2,8 +2,7 @@ param(
     [Parameter(Mandatory)][string]$RepoName,
     [Parameter(Mandatory)][string]$OrgName,
     [Parameter(Mandatory)][hashtable]$Keys,
-    [Parameter(Mandatory)][string]$DeviceDetection,
-    [Parameter(Mandatory)][string]$DeviceDetectionUrl,
+    [string]$IpIntelligenceUrl,
     [string]$Name,
     [string]$Version,
     [string]$Branch = "main", # this is actually the examples branch, but the name has to just be 'Branch' to be recognized by run-repo-script.ps1
@@ -11,13 +10,19 @@ param(
     [string]$ExamplesBranch = $Branch
 )
 
+# Skip integration tests if IpIntelligenceUrl is not provided
+if (!$IpIntelligenceUrl) {
+    Write-Output "Skipping integration tests - IpIntelligenceUrl not provided"
+    exit 0
+}
+
 $RepoPath = [IO.Path]::Combine($pwd, $RepoName)
 
 try {
     Write-Output "Cloning '$ExamplesRepo'"
     ./steps/clone-repo.ps1 -RepoName $ExamplesRepo -OrgName $OrgName -Branch $ExamplesBranch
-    & "./$ExamplesRepo/ci/fetch-assets.ps1" -RepoName $ExamplesRepo -DeviceDetection $DeviceDetection -DeviceDetectionUrl $DeviceDetectionUrl
-    
+    & "./$ExamplesRepo/ci/fetch-assets.ps1" -RepoName $ExamplesRepo -IpIntelligenceUrl $IpIntelligenceUrl
+
     Write-Output "Entering ip-intelligence-java directory"
     Push-Location $RepoPath
     # If the Version parameter is set to "0.0.0", set the Version variable to the version specified in the pom.xml file
