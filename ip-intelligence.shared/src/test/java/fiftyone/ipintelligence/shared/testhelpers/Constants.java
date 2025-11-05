ea16f067f83ad93f2daf93ced8ad39fc8db2196e
Oleksandr Shaitan <shaitanaleksander@gmail.com>
2025-11-05 20:36:06 +0100

    Merged Pull Request '#50 fix/Process_Hash_CaseInsensitiveKeys-test->main: Update test helper constants with new IPv6 address'
    Co-authored-by: shaitan <shaitanaleksender@gmail.com>

--- ip-intelligence.shared/src/test/java/fiftyone/ipintelligence/shared/testhelpers/Constants.java
+++ ip-intelligence.shared/src/test/java/fiftyone/ipintelligence/shared/testhelpers/Constants.java
@@ -27,5 +27,5 @@ public class Constants {
 
 
     public static final String IPV4_ADDRESS = "50.154.29.201";
-    public static final String IPV6_ADDRESS = "c50f:2ad9:2597:9255:5a6a:925b:5c41:5eca";
+    public static final String IPV6_ADDRESS = "2001:4860:4860::8888";
 }
