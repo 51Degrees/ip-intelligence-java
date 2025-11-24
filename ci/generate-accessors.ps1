50440947d46c540142ab7305956965e024474df7
Alexander Popel <alexander.popel@postindustria.com>
2025-11-24 11:52:54 +0200

    ci: update generate-accessors parameters

--- ci/generate-accessors.ps1
+++ ci/generate-accessors.ps1
@@ -1,10 +1,11 @@
 param (
     [Parameter(Mandatory)][string]$RepoName,
-    [string]$DataFile = "$PWD/$RepoName/51Degrees-EnterpriseV4.ipi"
+    [string]$MetaDataPath = "$PWD/common-metadata",
+    [string]$DataType = "IpIntelligence"
 )
 $ErrorActionPreference = "Stop"
 
-./tools/ci/generate-accessors.ps1 @PSBoundParameters
+./tools/ci/generate-accessors.ps1 -RepoName:$RepoName -MetaDataPath:$MetaDataPath -DataType:$DataType
 
 Copy-Item "tools/Java/IPIntelligenceDataBase.java" "ip-intelligence-java/ip-intelligence.shared/src/main/java/fiftyone/ipintelligence/shared/"
 Copy-Item "tools/Java/IPIntelligenceData.java" "ip-intelligence-java/ip-intelligence.shared/src/main/java/fiftyone/ipintelligence/shared/"
