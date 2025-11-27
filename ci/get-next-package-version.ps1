06064f41efb2bf6209773ce1abb897e2f0f753be
Alexander Popel <alexander.popel@postindustria.com>
2025-11-27 10:28:46 +0200

    ci: drop the alpha suffix in versioning

--- ci/get-next-package-version.ps1
+++ ci/get-next-package-version.ps1
@@ -2,6 +2,4 @@ param (
     [Parameter(Mandatory)][string]$RepoName,
     [string]$VariableName = "Version"
 )
-$ErrorActionPreference = "Stop"
-
-Set-Variable -Scope Global -Name $VariableName -Value (./steps/get-next-package-version.ps1 -RepoName $RepoName -Config GitVersion.yml)
+./java/get-next-package-version.ps1 -RepoName:$RepoName -VariableName:$VariableName
