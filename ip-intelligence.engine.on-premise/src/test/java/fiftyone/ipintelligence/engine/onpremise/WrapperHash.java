c1757fd2158511874f5959c20ffc1265f2855f8b
Yaroslav <Yaroslavvlasenko@gmail.com>
2026-01-29 19:02:30 +0200

    Merged Pull Request '#86 fix/wrapper-hash-data-update-service->main: TEST: Integrate DataUpdateService in WrapperHash with improved resource management'

--- ip-intelligence.engine.on-premise/src/test/java/fiftyone/ipintelligence/engine/onpremise/WrapperHash.java
+++ ip-intelligence.engine.on-premise/src/test/java/fiftyone/ipintelligence/engine/onpremise/WrapperHash.java
@@ -32,19 +32,24 @@ import fiftyone.pipeline.engines.fiftyone.data.ComponentMetaData;
 import fiftyone.pipeline.engines.fiftyone.data.FiftyOneAspectPropertyMetaData;
 import fiftyone.pipeline.engines.fiftyone.data.ProfileMetaData;
 import fiftyone.pipeline.engines.fiftyone.data.ValueMetaData;
+import fiftyone.pipeline.engines.services.DataUpdateService;
+import fiftyone.pipeline.engines.services.DataUpdateServiceDefault;
 import org.slf4j.ILoggerFactory;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
+import java.io.IOException;
 
 public class WrapperHash implements Wrapper {
 
     protected static final ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
     private Pipeline pipeline;
     private IPIntelligenceOnPremiseEngine engine;
+    private DataUpdateService dataUpdateService;
 
     public WrapperHash(File dataFile, Constants.PerformanceProfiles profile) throws Exception {
-        engine = new IPIntelligenceOnPremiseEngineBuilder(loggerFactory, null)
+        dataUpdateService = new DataUpdateServiceDefault();
+        engine = new IPIntelligenceOnPremiseEngineBuilder(loggerFactory, dataUpdateService)
                 .setPerformanceProfile(profile)
                 .setAutoUpdate(false)
                 .build(dataFile.toString(), false);
@@ -84,16 +89,30 @@ public class WrapperHash implements Wrapper {
     @Override
     public void close() {
         try {
-            pipeline.close();
+            if (pipeline != null) {
+                pipeline.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         pipeline = null;
+
         try {
-            engine.close();
+            if (engine != null) {
+                engine.close();
+            }
         } catch (Exception e) {
             e.printStackTrace();
         }
         engine = null;
+
+        try {
+            if (dataUpdateService != null) {
+                dataUpdateService.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        dataUpdateService = null;
     }
 }
